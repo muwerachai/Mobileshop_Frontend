98338d0427ba3c05b2189125b2ecc2902221dbc3
werachai <>
2024-09-06 20:31:51 +0700

    feat:head-catbar

--- src/Router/Router.js
+++ src/Router/Router.js
@@ -1,15 +1,15 @@
 import { Link, Route, Routes, useNavigate } from "react-router-dom";
-import AdminLayout from "../pages/AdminLayout";
 import Clientlayout from "../pages/Clientlayout";
 import Landingpage from "../pages/Userpages/Landingpage";
+import DevLayout from "../components/Admin/DevLayout";
+
 function Router() {
   return (
     <Routes>
       <Route path="/" element={<Clientlayout />}>
         <Route path="" element={<Landingpage />} />
       </Route>
-      <Route path="/seller" element={<AdminLayout />}></Route>
-      <Route path="/admin" element={<AdminLayout />}></Route>
+      <Route path="/admin" element={<DevLayout />}></Route>
       <Route path="*" element={useNavigate("/")} />
     </Routes>
   );
---
+++ src/components/Admin/DevLayout.jsx
@@ -0,0 +1,4 @@
+function DevLayout() {
+  return <div></div>;
+}
+export default DevLayout;
